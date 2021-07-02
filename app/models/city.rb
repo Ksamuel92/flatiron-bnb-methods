4d7f88fd53e6b424c70b94392ed1b70a0616b01c
Kyle Samuel <Ksamuel92@gmail.com>
2021-07-02 12:03:16 -0400

    commit tree 1d1339628fa64d4ab5b728aa27bb17cf0ca89ec1

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,14 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, through: :listings
+
+  include Reservable
+
+  def city_openings(start_date, end_date)
+    booked_listings = "select distinct(l.id) FROM cities c JOIN neighborhoods n on c.id = n.city_id JOIN listings l on n.id = l.neighborhood_id JOIN reservations r on l.id = r.listing_id WHERE r.checkin >= #{start_date} AND r.checkout <= #{end_date}"
+    Listing.find_by_sql("select l.id, l.title FROM cities c JOIN neighborhoods n on c.id = n.city_id JOIN listings l on n.id = l.neighborhood_id WHERE l.id NOT IN (#{booked_listings})")
+  end
 
 end
 
